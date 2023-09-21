56d5ccf155fa8b6945ab988db5b51f241125c2f3
Mehmet <mehmet.h.yurdakul@gmail.com>
2023-09-21 22:18:51 +0200

    added basic-ish validation plus more styling

--- scripts/formHandler.js
+++ scripts/formHandler.js
@@ -1,16 +1,56 @@
 const form = document.querySelector("form");
+const inputElements = document.querySelectorAll("input");
+
+inputElements.forEach((inputElement) => {
+  inputElement.addEventListener("change", () => {
+    if (inputElement.value.trim() !== "") {
+      inputElement.classList.add("input-good");
+    } else {
+      inputElement.classList.remove("input-good");
+    }
+  });
+});
+
+// Add a blur event listener to remove the class when the input loses focus
+inputElements.forEach((inputElement) => {
+  inputElement.addEventListener("blur", () => {
+    if (inputElement.value.trim() !== "") {
+      inputElement.classList.add("input-good");
+    } else {
+      inputElement.classList.remove("input-good");
+    }
+  });
+});
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const data = new FormData(form);
-  const country = data.get("country");
-  const title = data.get("title");
+  let country = data.get("country");
+  let title = data.get("title");
   const link = data.get("link");
   const arrivalDate = data.get("arrival_date");
   const departureDate = data.get("departure_date");
   const image = data.get("image");
   const description = data.get("description");
+
+  // Transform the country to uppercase
+  country = country.toUpperCase();
+
+  // Capitalize the first letter of the title
+  title = title.charAt(0).toUpperCase() + title.slice(1);
+
+  // Check if the link starts with the allowed prefixes
+  if (
+    !link.startsWith("https://www.google.com/maps/") &&
+    !link.startsWith("https://maps.app.goo.gl/")
+  ) {
+    const errorMessage = "Only Google Maps links allowed.";
+    document.getElementById("error-message").textContent = errorMessage;
+    return; // Prevent form submission
+  } else {
+    document.getElementById("error-message").textContent = ""; // Clear any previous error message
+  }
 
   const formattedArrival = dateFormatter(arrivalDate);
   const formattedDeparture = dateFormatter(departureDate);
@@ -42,12 +82,11 @@ form.addEventListener("submit", (e) => {
         form.reset();
       }
     })
-
     .catch((err) => console.error(err));
 });
 
 function dateFormatter(date) {
-  // Convert the date string "DD,MM,YYYY" to an array of ["DD", "MM", "YYYY"]
+  // Convert the date string "YYYY-MM-DD" to an array of ["YYYY", "MM", "DD"]
   const inputDate = date.split("-");
 
   const monthNames = [
@@ -61,13 +100,14 @@ function dateFormatter(date) {
     "Aug",
     "Sep",
     "Oct",
-    "Nove",
+    "Nov",
     "Dec",
   ];
+
   // Format the date as "DD month, YYYY"
-  const formattedDate = `${inputDate[2]} ${monthNames[Number(inputDate[1])]}, ${
-    inputDate[0]
-  }`;
+  const formattedDate = `${inputDate[2]} ${
+    monthNames[Number(inputDate[1]) - 1]
+  }, ${inputDate[0]}`;
 
   return formattedDate;
 }
